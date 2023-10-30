eb90c24cddc4558c57f8396881ee3fe8124ffee2
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-30 02:21:59 +0000

    clean up iam service to use db

--- internal/iam/http/service.go
+++ internal/iam/http/service.go
@@ -1,25 +1,29 @@
 package http
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/adoublef/past-swift/fly"
+	"github.com/adoublef/past-swift/internal/iam"
+	"github.com/adoublef/past-swift/internal/iam/sqlite3"
 	"github.com/adoublef/past-swift/internal/sessions"
 	o2 "github.com/adoublef/past-swift/oauth2"
 	"github.com/adoublef/past-swift/oauth2/github"
+	s3 "github.com/adoublef/past-swift/sqlite3"
 	"github.com/go-chi/chi/v5"
-	"github.com/rs/xid"
 )
 
 var _ http.Handler = (*Service)(nil)
 
 type Service struct {
-	m *chi.Mux
-	a *o2.Authenticator
-	s *sessions.Session
+	m  *chi.Mux
+	a  *o2.Authenticator
+	db *sql.DB
 }
 
 // ServeHTTP implements http.Handler.
@@ -27,14 +31,18 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.m.ServeHTTP(w, r)
 }
 
-func New(sessions *sessions.Session) *Service {
+func New(dsn string) (*Service, error) {
+	db, err := s3.Open(dsn)
+	if err != nil {
+		return nil, err
+	}
 	s := Service{
-		m: chi.NewMux(),
-		a: &o2.Authenticator{},
-		s: sessions,
+		m:  chi.NewMux(),
+		a:  &o2.Authenticator{},
+		db: db,
 	}
 	s.routes()
-	return &s
+	return &s, nil
 }
 
 func (s *Service) routes() {
@@ -67,6 +75,7 @@ func (s *Service) handleSignIn() http.HandlerFunc {
 		c, _ := s.a.Configs().Get("github")
 		url, err := s.a.SignIn(w, r, c)
 		if err != nil {
+			log.Println(err)
 			http.Error(w, "Failed to create auth code url", http.StatusInternalServerError)
 			return
 		}
@@ -76,15 +85,37 @@ func (s *Service) handleSignIn() http.HandlerFunc {
 
 func (s *Service) handleCallback() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ctx     = r.Context()
+			session = sessions.FromContext(ctx)
+			db      = s.db
+		)
 		c, _ := s.a.Configs().Get("github")
-		ou, err := s.a.HandleCallback(w, r, c)
+		info, err := s.a.HandleCallback(w, r, c)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "Failed to complete auth flow", http.StatusUnauthorized)
 			return
 		}
-		fmt.Println(ou)
-		_, err = s.s.Site().Set(w, r, xid.New())
+		u := iam.NewUser(info.ID, info.Login, info.Photo, info.Name)
+		found, err := sqlite3.ExistingProfile(ctx, db, info.ID)
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			if err := sqlite3.RegisterUser(ctx, db, u); err != nil {
+				http.Error(w, "Failed to register user", http.StatusInternalServerError)
+				return
+			}
+		case err != nil:
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		// id
+		var id = u.ID()
+		if found != nil {
+			id = found.ID
+		}
+		_, err = session.Site().Set(w, r, id)
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "Failed to set site cookie", http.StatusInternalServerError)
@@ -96,8 +127,12 @@ func (s *Service) handleCallback() http.HandlerFunc {
 
 func (s *Service) handleSignOut() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ctx     = r.Context()
+			session = sessions.FromContext(ctx)
+		)
 		// delete site
-		_ = s.s.Site().Delete(w, r)
+		go session.Site().Delete(w, r)
 		// redirect to home
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
